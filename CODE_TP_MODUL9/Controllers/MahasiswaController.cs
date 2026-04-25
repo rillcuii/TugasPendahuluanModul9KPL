b66ea84e20ff0fda5cd32c298d42d977d3b98777
ariel <fadillah0777@gmail.com>
2026-04-25 21:05:13 +0700

    menyelesaikan tp modul 9

---
+++ CODE_TP_MODUL9/Controllers/MahasiswaController.cs
@@ -0,0 +1,46 @@
+﻿using CODE_TP_MODUL9.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CODE_TP_MODUL9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> dataMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa("ARIEL", "103082430001"),
+            new Mahasiswa("ANDRA", "10302000002"),
+            new Mahasiswa("LEVI", "10302000003")
+        };
+
+        // A. GET: api/Mahasiswa
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return dataMahasiswa;
+        }
+
+        // B. POST: api/Mahasiswa
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mhs)
+        {
+            dataMahasiswa.Add(mhs);
+        }
+
+        // C. GET: api/Mahasiswa/{id}
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return dataMahasiswa[id];
+        }
+
+        // D. DELETE: api/Mahasiswa/{id}
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            dataMahasiswa.RemoveAt(id);
+        }
+    }
+}
---
+++ CODE_TP_MODUL9/Models/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿namespace CODE_TP_MODUL9.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+
+        public Mahasiswa(string nama, string nim)
+        {
+            Nama = nama;
+            Nim = nim;
+        }
+    }
+}
